81d4ff40d919efd15bac1fb41d4c9c6f28872699
yaoxing <yaoxing@douban.com>
2015-07-31 15:00:23 +0800

    move files

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	//"strings"
+	"log"
+)
+
+var cnt int
+
+func hi(w http.ResponseWriter, r *http.Request) {
+//	r.ParseForm()
+//	fmt.Println(r.Form)
+//	fmt.Println("path", r.URL.Path)
+//	fmt.Println("scheme", r.URL.Scheme)
+//	for k, v := range(r.Form) {
+//		fmt.Println("key: ", k)
+//		fmt.Println("value: ", strings.Join(v, ""))
+//	}
+//
+	fmt.Fprintln(w, "hello")
+	cnt += 1
+}
+
+func main() {
+	cnt = 0
+	http.HandleFunc("/", hi)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
